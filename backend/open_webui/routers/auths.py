92c717d018e8f634a6608d75a9197dcca8e1b41f
司鹏举 <1729053462@qq.com>
2025-07-05 09:45:54 +0800

    手机号登录添加微信信息

--- backend/open_webui/routers/auths.py
+++ backend/open_webui/routers/auths.py
@@ -1107,6 +1107,12 @@ async def sms_signin(request: Request, response: Response, form_data: SMSLoginFo
         "profile_image_url": user.profile_image_url,
         "permissions": user_permissions,
         "credit": credit.credit,
+        "primary_login_type": user.primary_login_type,
+        "available_login_types": user.available_login_types,
+        "phone_number": user.phone_number,
+        "wechat_openid": user.wechat_openid,
+        "wechat_nickname": user.wechat_nickname,
+        "binding_status": user.binding_status,
     }
 
 
